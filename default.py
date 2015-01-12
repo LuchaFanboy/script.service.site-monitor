fea138116c88303b8f8af28d25708cca9b75f99d
Vicente Obregon <desarrollogis@gmail.com>
2015-01-12 01:28:25 -0700

    check

--- default.py
+++ default.py
@@ -1,3 +1,16 @@
-import xbmc, xbmcaddon
+import xbmc, xbmcaddon, time, os
 
-xbmc.log("Starting site monitor...", xbmc.LOGDEBUG)
+addon_name = 'script.service.site-monitor'
+xbmc.log(addon_name + ': Starting site monitor...', xbmc.LOGDEBUG)
+while not xbmc.abortRequested:
+	server1 = xbmcaddon.Addon().getSetting('server1')
+	if server1 != '':
+		xbmc.log(addon_name + ': Review ' + server1, xbmc.LOGDEBUG)
+		response = os.system('ping -c 1 ' + server1)
+		if response == 0:
+			xbmc.log(addon_name + ': Up ' + server1, xbmc.LOGDEBUG)
+		else:
+			xbmc.log(addon_name + ': Down ' + server1, xbmc.LOGDEBUG)
+			song = xbmcaddon.Addon().getSetting('song')
+			xbmc.Player().play(song)
+	time.sleep(60)
