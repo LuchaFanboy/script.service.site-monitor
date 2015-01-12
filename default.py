9c3e2325bd185c4049f70b7bcaa16494e1f24b79
Vicente Obregon <desarrollogis@gmail.com>
2015-01-12 07:18:38 -0700

    notify

--- default.py
+++ default.py
@@ -11,12 +11,15 @@ while not xbmc.abortRequested:
 	server1 = __addon__.getSetting('server1')
 	if server1 == '':
 		continue
-	line1 = 'Checking %s ...' % (server1)
-	xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
+	notify = __addon__.getSetting('notify')
+	if notify:
+		line1 = 'Checking %s ...' % (server1)
+		xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
 	response = os.system('ping -c 1 ' + server1)
 	if response == 0:
-		line1 = '%s is up.' % (server1)
-		xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
+		if notify:
+			line1 = '%s is up.' % (server1)
+			xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
 	else:
 		line1 = '%s is down.' % (server1)
 		xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
