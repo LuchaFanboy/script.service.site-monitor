8d4d4992dd3c41f89f9ef3c3bcc94a3a3abd39a9
Vicente Obregon <desarrollogis@gmail.com>
2015-01-12 06:48:12 -0700

    arrangement

--- default.py
+++ default.py
@@ -9,16 +9,17 @@ line1 = 'Starting site monitor...'
 xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
 while not xbmc.abortRequested:
 	server1 = __addon__.getSetting('server1')
-	if server1 != '':
-		line1 = 'Checking %s ...' % (server1)
+	if server1 == '':
+		continue
+	line1 = 'Checking %s ...' % (server1)
+	xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
+	response = os.system('ping -c 1 ' + server1)
+	if response == 0:
+		line1 = '%s is up.' % (server1)
 		xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
-		response = os.system('ping -c 1 ' + server1)
-		if response == 0:
-			line1 = '%s is up.' % (server1)
-			xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
-		else:
-			line1 = '%s is down.' % (server1)
-			xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
-			song = __addon__.getSetting('song')
-			xbmc.Player().play(song)
+	else:
+		line1 = '%s is down.' % (server1)
+		xbmcgui.Dialog().notification(__addonname__, line1, __icon__, __time__)
+		song = __addon__.getSetting('song')
+		xbmc.Player().play(song)
 	time.sleep(60)
