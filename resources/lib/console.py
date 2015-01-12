c309b79a8cd341f544e6e3d100f3cb2a00c5dbea
Vicente Obregon <desarrollogis@gmail.com>
2015-01-12 16:23:44 -0700

    console

---
+++ resources/lib/console.py
@@ -0,0 +1,44 @@
+import signal, sys;
+
+__should_exit__ = False
+
+def handler(signum, frame):
+	global __should_exit__
+	__should_exit__ = True
+
+class Model(object):
+	def __init__(self):
+		signal.signal(signal.SIGINT, handler)
+
+	def notification(self, line1):
+		print line1
+
+	def abortRequested(self):
+		return __should_exit__
+
+	def read(self):
+		pass
+
+	def hasChanged(self):
+		return False
+
+	def getServer(self):
+		return 'home.vicenteobregon.com'
+
+	def getCheck(self):
+		return 1
+
+	def getNotify(self):
+		return True
+
+	def getSong(self):
+		return '\a'
+
+	def play(self, song):
+		print song
+
+	def getSleep(self, error):
+		if error:
+			return 1
+		else:
+			return 60
